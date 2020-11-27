63bab6e6b3406b0ca2df87aa8359d88d583a1901
unknown <zirzirlove96@github.com>
2020-11-27 16:00:27 +0900

    5th Challenge

--- Challenge/5th Day/src/index.js
+++ Challenge/5th Day/src/index.js
@@ -2,15 +2,17 @@
 
 // You're gonna need this
 const NINE_HOURS_MILLISECONDS = 32400000;
+const KSThours = NINE_HOURS_MILLISECONDS/(60*60*1000);
+//UTC시간에서 KST시간으로 구하기 위해
 const div = document.querySelector(".js-clock");
 const clock = div.querySelector("h2");
 
 function getTime() {
   // Don't delete this.
-  const xmasDay = new Date("2020-12-24:00:00:00+0900");
+  const xmasDay = new Date("2020-12-3:00:00:00+0900");
   const date = new Date();
-  const day = (xmasDay.getDate()-date.getUTCDate())+31//NINE_HOURS_MILLISECONDS
-  const hours = 23-date.getHours();//(date.getHours()-xmasDay.getHours());
+  const day = (xmasDay.getDate()-date.getUTCDate())+31
+  const hours = 23-(date.getUTCHours()+KSThours);
   const minutes = 59-(date.getMinutes()-xmasDay.getMinutes());
   const seconds = 59-(date.getSeconds()-xmasDay.getSeconds());
 
@@ -18,6 +20,8 @@ function getTime() {
                      ${hours < 10 ? `0${hours}` : hours}h  
                       ${minutes < 10 ? `0${minutes}` : minutes}m  
                       ${seconds < 10 ? `0${seconds}` : seconds}s`;
+
+ 
 
 }
 
